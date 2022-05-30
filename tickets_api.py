8b2601be8b31c73b6d5d4951628ee88eb3294b99
Your Name <you@example.com>
2022-05-30 15:47:14 -0400

    "delete"

--- tickets_api.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-key = '1g89Fx2KHiAD3WdLaBFtpKdTxZEW2lvS'
-
-response = requests.get('https://app.ticketmaster.com/discovery/v2/events?.json?{key}' , 
-                        params= {
-                                 'keyword':'concert'})
